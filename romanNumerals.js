fd675d2bb3c8bc895b2f928ff7d04a16e6f719c8
ksbleek <kyle.s.bleeker@gmail.com>
2024-06-05 19:49:35 -0500

    Made roman numerals object and array

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,6 +1,22 @@
 function toRomanLazy(num) {
-  return "";
+  let output = "";
+  const romanNumeralToArabic = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    IX: 9,
+    VIII: 8,
+    VII: 7,
+    VI: 6,
+    V: 5,
+    IV: 4,
+    III: 3,
+    II: 2,
+    I: 1;
 }
+let romanNumeralPriorityOrder = [M, D, C, L, X, IX, VIII, VII, VI, V, IV, III, II, I]
 
 function toRoman(num) {
   return "";
